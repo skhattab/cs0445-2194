a1f96f07f80d88f2902545084381e95b192bc177
skhattab <skhattab@cs.pitt.edu>
2019-01-29 18:17:05 -0500

    lecture 7 (1/29/2019)

--- BagADT/BagTest.java
+++ BagADT/BagTest.java
@@ -8,8 +8,8 @@ import java.util.Arrays;
 public class BagTest {
    public static void main(String[] args) {
      BagInterface<String> stringBag = new LinkedBag<>();
-     stringBag.add("String");
-     stringBag.add("String");
+     stringBag.add("String1");
+     stringBag.add("String2");
      System.out.println("Bag size is " + stringBag.getCurrentSize());
      Object[] strings = stringBag.toArray();
      System.out.println(Arrays.toString(strings));
@@ -17,9 +17,20 @@ public class BagTest {
      System.out.println("String in bag? " + stringBag.contains("String"));
      System.out.println("String appears " +
                    stringBag.getFrequencyOf("String") + " times in the bag.");
-     // stringBag.remove();
-     // System.out.println("String appears " +
-     //              stringBag.getFrequencyOf("String") + " times in the bag.");
+     stringBag.remove();
+     System.out.println("String1 appears " +
+                   stringBag.getFrequencyOf("String1") +
+                   " times in the bag.\n" +
+                   "String2 appears " +
+                   stringBag.getFrequencyOf("String2") +
+                   " times in the bag.");
+    stringBag.remove("String1");
+    System.out.println("String1 appears " +
+                  stringBag.getFrequencyOf("String1") +
+                  " times in the bag.\n" +
+                  "String2 appears " +
+                  stringBag.getFrequencyOf("String2") +
+                  " times in the bag.");
 
 
    }
--- BagADT/LinkedBag.java
+++ BagADT/LinkedBag.java
@@ -1,133 +1,201 @@
 public final class LinkedBag<T> implements BagInterface<T>{
-  Node firstNode;
-  int numberOfItems;
+Node firstNode;
+int numberOfItems;
 
-  public LinkedBag(){
-    firstNode = null;
-    numberOfItems = 0;
-  }
-  /** Checks if the bag is empty
-   * @return true if the bag is empty; false otherwise.
-   */
-  public boolean isEmpty() {
-    return numberOfItems == 0;
-  }
+public LinkedBag(){
+  firstNode = null;
+  numberOfItems = 0;
+}
+/** Checks if the bag is empty
+ * @return true if the bag is empty; false otherwise.
+ */
+public boolean isEmpty() {
+  return numberOfItems == 0;
 
-  /** Returns the number of items in the bag
-   * @return the integer number of items in the bag
-   */
-  public int getCurrentSize(){
-    return numberOfItems;
-  }
+}
 
-  /** Adds an item to the bag if possible
-   * @param item The new item to be added
-   * @return true if adding succeeds or false if the bag is full
-   */
-  public boolean add(T item){
-    // Step 1
-    Node newNode = new Node(item);
-    //Step 2
-    newNode.next = firstNode;
-    //Step 3
-    firstNode = newNode;
-    numberOfItems++;
-    //firstNode = new Node(item, firstNode);
-    return true;
-  }
+/** Returns the number of items in the bag
+ * @return the integer number of items in the bag
+ */
+public int getCurrentSize(){
+  return numberOfItems;
+}
 
-  /** Removes an unspecified item from the bag if possible
-   * @return the item that is removed or null if the bag is empty
-   */
-  public T remove(){
-    return null;
-  }
+/** Adds an item to the bag if possible
+ * @param item The new item to be added
+ * @return true if adding succeeds or false if the bag is full
+ */
+public boolean add(T item){
+  // Step 1
+  Node newNode = new Node(item);
+  //Step 2
+  newNode.next = firstNode;
+  //Step 3
+  firstNode = newNode;
+  numberOfItems++;
+  //firstNode = new Node(item, firstNode);
+  return true;
+}
 
-  /** Searches for an item inside the bag
-   * @param item The item that we are searching for
-   * @return true if the item exists in the bag and false otherwise
-   */
-  public boolean contains(T item){
-    boolean result = false;
+/** Removes an unspecified item from the bag if possible
+ * @return the item that is removed or null if the bag is empty
+ */
+public T remove(){
+  T result = null;
+  if(!isEmpty()){
+    result = firstNode.data;
+    firstNode = firstNode.next;
+  }
+  return result;
+}
+
+/** Searches for an item inside the bag
+ * @param item The item that we are searching for
+ * @return true if the item exists in the bag and false otherwise
+ */
+public boolean contains(T item){
+  /*boolean result = false;
+  Node currentNode = firstNode;
+  while(currentNode != null){
+    if(item.equals(currentNode.data)){
+      result = true;
+      break;
+    }
+    currentNode = currentNode.next;
+  }
+  return result;
+  */
+
+  return (getReferenceTo(item) != null);
+}
+
+/** Removes one copy of item from the bag
+ * @param item The item to be removed
+ * @return true if the removal is successful; false if item doesn't exist in the bag
+ */
+public boolean remove(T item){
+  boolean result = false;
+  if(!isEmpty()){
+    Node currentNode = firstNode;
+    if(item.equals(firstNode.data)){
+      remove();
+      result = true;
+    } else {
+      while(currentNode.next != null){
+        if(item.equals(currentNode.next.data)){
+            currentNode.next = currentNode.next.next;
+            result = true;
+            break;
+        }
+        currentNode = currentNode.next;
+      }
+    }
+  }
+  return result;
+
+
+  /* Simpler solution */
+  /*boolean result = false;
+  if(!isEmpty()){
     Node currentNode = firstNode;
     while(currentNode != null){
       if(item.equals(currentNode.data)){
-        result = true;
-        break;
+         currentNode.data = firstNode.data;
+         remove();
+         result = true;
+         break;
       }
       currentNode = currentNode.next;
     }
-    return result;
   }
+  return result;
+  */
 
-  /** Removes one copy of item from the bag
-   * @param item The item to be removed
-   * @return true if the removal is successful; false if item doesn't exist in the bag
-   */
-  public boolean remove(T item){
-    return false;
+  /* Simpler solution using getReferenceTo */
+  /*boolean result = false;
+  Node toRemove = getReferenceTo(item);
+  if(toRemove != null){
+       toRemove.data = firstNode.data;
+       remove();
+       result = true;
   }
+  return result;
+  */
+}
 
-  /** Returns the number of copies of item that exist in the bag
-   * @param item The item that we search for
-   * @return the integer number of copies of item in the bag
-   */
-  public int getFrequencyOf(T item){
-    int result = 0;
-    Node currentNode = firstNode;
-    while(currentNode != null){
-      if(item.equals(currentNode.data)){
-        result++;
-      }
-      currentNode = currentNode.next;
+/** Returns the number of copies of item that exist in the bag
+ * @param item The item that we search for
+ * @return the integer number of copies of item in the bag
+ */
+public int getFrequencyOf(T item){
+  int result = 0;
+  Node currentNode = firstNode;
+  while(currentNode != null){
+    if(item.equals(currentNode.data)){
+      result++;
     }
-    return result;
+    currentNode = currentNode.next;
+  }
+  return result;
+}
+
+/** Returns an array that contains all the items in the bag
+ * @return a T[] of the items in the bag
+ */
+public T[] toArray(){
+  @SuppressWarnings("unchecked")
+  T[] result = (T[]) new Object[numberOfItems];
+
+  Node currentNode = firstNode;
+  int index = 0;
+  while((currentNode != null)
+        &&(index < numberOfItems)){
+    result[index] = currentNode.data;
+    index++;
+    currentNode = currentNode.next;
   }
 
-  /** Returns an array that contains all the items in the bag
-   * @return a T[] of the items in the bag
-   */
-  public T[] toArray(){
-    @SuppressWarnings("unchecked")
-    T[] result = (T[]) new Object[numberOfItems];
+  // for(Node currentNode = firstNode, int index = 0;
+  //     currentNode!= null && index < numberOfItems;
+  //     currentNode = currentNode.next, index++){
+  //       result[index] = currentNode.data;
+  // }
+  return result;
+}
 
-    Node currentNode = firstNode;
-    int index = 0;
-    while((currentNode != null)
-          &&(index < numberOfItems)){
-      result[index] = currentNode.data;
-      index++;
-      currentNode = currentNode.next;
+/** Removes all items from the bag
+ *  Garbage collector collects the old nodes
+ */
+public void clear(){
+  firstNode = null;
+}
+
+private Node getReferenceTo(T item){
+  Node result = null;
+  Node currentNode = firstNode;
+  while(currentNode != null){
+    if(item.equals(currentNode.data)){
+      result = currentNode;
+      break;
     }
+    currentNode = currentNode.next;
+  }
+  return result;
+}
 
-    // for(Node currentNode = firstNode, int index = 0;
-    //     currentNode!= null && index < numberOfItems;
-    //     currentNode = currentNode.next, index++){
-    //       result[index] = currentNode.data;
-    // }
-    return result;
+private class Node {
+  T data;
+  Node next;
+
+  private Node(T data){
+    this.data = data;
+    next = null;
   }
 
-  /** Removes all items from the bag
-   *
-   */
-  public void clear(){
-
+  private Node (T data, Node next){
+    this.data = data;
+    this.next = next;
   }
-
-  private class Node {
-    T data;
-    Node next;
-
-    private Node(T data){
-      this.data = data;
-      next = null;
-    }
-
-    private Node (T data, Node next){
-      this.data = data;
-      this.next = next;
-    }
-  }
+}
 
 }
