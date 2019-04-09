1933e65be2863f37473714a4dd954d6db6abe439
skhattab <skhattab@cs.pitt.edu>
2019-04-09 12:56:54 -0400

    Lecture 23 (4/4/2019)

---
+++ TreeADT/BinaryNode.java
@@ -0,0 +1,76 @@
+public class BinaryNode<T> {
+  private T data;
+  private BinaryNode<T> left;
+  private BinaryNode<T> right;
+
+  public BinaryNode(T data){
+    this(data, null, null);
+  }
+
+  public BinaryNode(T data, BinaryNode<T> left,
+                   BinaryNode<T> right){
+      this.data = data;
+      this.left = left;
+      this.right = right;
+  }
+
+  public void setData(T data){
+    this.data = data;
+  }
+
+  public T getData(){
+    return data;
+  }
+
+  public void setLeft(BinaryNode<T> left){
+    this.left = left;
+  }
+
+  public BinaryNode<T> getLeft(){
+    return left;
+  }
+
+  public void setRight(BinaryNode<T> right){
+    this.right = right;
+  }
+
+  public BinaryNode<T> getRight(){
+    return right;
+  }
+
+  public int getNumberOfNodes(){
+    int result = 1;
+    if(left != null){
+      result += left.getNumberOfNodes();
+    }
+    if(right != null){
+      result += right.getNumberOfNodes();
+    }
+    return result;
+  }
+
+  public int getHeight(){
+    int max = 0;
+    if(left != null){
+      max = left.getHeight();
+    }
+    if(right != null){
+      if(right.getHeight() > max){
+        max = right.getHeight();
+      }
+    }
+
+    return 1 + max;
+  }
+
+  public BinaryNode<T> copy(){
+    BinaryNode<T> newRoot = new BinaryNode<>(data);
+    if(left != null){
+      newRoot.left = left.copy();
+    }
+    if(right != null){
+      newRoot.right = right.copy();
+    }
+    return newRoot;
+  }
+}
